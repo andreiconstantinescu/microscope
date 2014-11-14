291c1136fe5ce80d33f92cdb9cc8f4cb0882d7b0
Andrei Constantinescu <andrei.c@icloud.com>
2014-11-14 13:24:53 +0200

    Add uniqueness to urls.

--- client/templates/posts/post_submit.js
+++ client/templates/posts/post_submit.js
@@ -11,7 +11,10 @@ Template.postSubmit.events({
       if (error) {
         return alert(error.reason);
       }
+
+      if(result.postExists)
+        alert('This link has already been posted.');
+      Router.go('postPage', post);
     });
-    Router.go('postPage', post);
   }
 });
